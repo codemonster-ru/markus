a34e5d4d51fdf08ab9241c96210e09e36026577b
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-09-07 12:53:34 +0700

    Corrected getHtml function in converter class (replace with string or function).

--- src/core/converter.ts
+++ src/core/converter.ts
@@ -7,7 +7,14 @@ export default class {
         Object.keys(rules).map((key: string) => {
             const rule: RuleType = rules[key] as RuleType;
 
-            content = content.replaceAll(rule.md.search, rule.md.replace);
+            if (typeof rule.md.replace === 'string') {
+                content = content.replaceAll(rule.md.search, rule.md.replace);
+            } else if (typeof rule.md.replace === 'function') {
+                content = content.replaceAll(
+                    rule.md.search,
+                    rule.md.replace as (substring: string, ...args: any[]) => string,
+                );
+            }
         });
 
         return content;
